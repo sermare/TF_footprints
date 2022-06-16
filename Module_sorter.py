aec6946f5900790ba41eda922bb241ffbd4eb122
sermare <sergio.mares@okstate.edu>
2022-06-16 01:05:03 -0700

    testing

--- Module_sorter.py
+++ Module_sorter.py
@@ -7,5 +7,11 @@ entries = os.listdir(cur_dir)
 
 print("Found %s entries in %s" % (len(entries), cur_dir))
 print('-' * 10)
-for entry in entries:
-    print(entry)
+
+[print(entry) if '.py' in entry in entries]
+
+# footprint = pd.read_csv()
+
+
+# TF_footprints = pd.read_csv('P503_5.ATAC.peaks.TF.info.txt', "\t")
+# TF_footprints = TF_footprints.sort_values(by ="Chr")
