67af2101eb41ffb55c014a33d8c9b944e26cb3c6
sermare <sergio.mares@okstate.edu>
2022-06-17 17:00:38 -0700

    description of footprints

--- Module_sorter.py
+++ Module_sorter.py
@@ -20,6 +20,7 @@ extension_txt = 'txt'
 os.chdir(path)
 result = glob.glob('*.{}'.format(extension_csv))
 
+
 modules = pd.read_csv(result[0])
 modules = modules.sort_values(by = ['chr','start'])
 
@@ -27,47 +28,50 @@ modules = modules.sort_values(by = ['chr','start'])
 
 result_2 = glob.glob('*.{}'.format(extension_txt))
 
-footprint = pd.read_csv(result_2[0], "\t")
-footprint = footprint.sort_values(by ="Chr")
+for file in result_2:
 
-def closest(list1, list2, k):
-    return(list1[np.abs(list1 - list2[k]).argmin()])
+    print(file)
+    footprint = pd.read_csv(file, "\t")
+    footprint = footprint.sort_values(by ="Chr")
+
+    def closest(list1, list2, k):
+        return(list1[np.abs(list1 - list2[k]).argmin()])
 
 
-### Loading the chromosomes
+    ### Loading the chromosomes
 
-chromosomes = ['chr1', 'chrY', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15',
-       'chr16', 'chr17', 'chr18', 'chr19', 'chr2', 'chr20', 'chr21',
-       'chr22', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9',
-       'chrX']  
+    chromosomes = ['chr1', 'chrY', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15',
+        'chr16', 'chr17', 'chr18', 'chr19', 'chr2', 'chr20', 'chr21',
+        'chr22', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9',
+        'chrX']  
 
-## Where the magic happens
+    ## Where the magic happens
 
-for chromosome in chromosomes:
+    for chromosome in chromosomes[:1]:
 
-    try:
-        tmp_modules = modules[modules.chr == chromosome]
-        tmp_modules = tmp_modules.reset_index()
+        try:
+            tmp_modules = modules[modules.chr == chromosome]
+            tmp_modules = tmp_modules.reset_index()
 
-        tmp_tf_footprints = footprint[footprint.Chr == chromosome]
-        tmp_tf_footprints = tmp_tf_footprints.reset_index()
+            tmp_tf_footprints = footprint[footprint.Chr == chromosome]
+            tmp_tf_footprints = tmp_tf_footprints.reset_index()
 
-        for i, y in enumerate(tmp_tf_footprints['tf_c1']):
-            x1 = closest(tmp_modules['start'], tmp_tf_footprints['tf_c1'], i)
-            x2 = tmp_modules[tmp_modules['start'] == x1]['end'].iloc[-1]
+            for i, y in enumerate(tmp_tf_footprints['tf_c1']):
+                x1 = closest(tmp_modules['start'], tmp_tf_footprints['tf_c1'], i)
+                x2 = tmp_modules[tmp_modules['start'] == x1]['end'].iloc[-1]
 
-            if (x1 < y < x2):
-                tmp_tf_footprints['Unnamed: 9'][i] = tmp_modules[tmp_modules['start'] == x1]['Module'].iloc[-1]
+                if (x1 < y < x2):
+                    tmp_tf_footprints['Unnamed: 9'][i] = tmp_modules[tmp_modules['start'] == x1]['Module'].iloc[-1]
 
-        if chromosome == 'chr1':
-            out_csv = tmp_tf_footprints
-        else:
-            out_csv = pd.concat([out_csv, tmp_tf_footprints])
-    except:
-        pass
+            if chromosome == 'chr1':
+                out_csv = tmp_tf_footprints
+            else:
+                out_csv = pd.concat([out_csv, tmp_tf_footprints])
+        except:
+            pass
 
-out_csv.to_csv('output.tsv')
+    out_csv.to_csv(file + '_output.tsv')
 
-### Time it for fun
-end = time.time()
-print(end - start)
+    ### Time it for fun
+    end = time.time()
+    print(end - start)
